ee5b16e73820cb99f500eb584984291917a269ab
Handsomer <sun_pro12580@163.com>
2020-02-26 16:50:09 +0800

    输出统计重复行数，重复内容，以及文件名

--- cp1/case.go
+++ cp1/case.go
@@ -23,6 +23,43 @@ func cal_nums(){
 	}
 }
 
+func dup_files(){
+	counts := make(map[string] int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countlines(os.Stdin, counts)
+		out_counts(counts, os.Args[0])
+	}else{
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v /n", err)
+			}
+			countlines(f, counts)
+			f.Close()
+			out_counts(counts, arg)
+			// 有意思的是，Go语言中并没有为 map 提供任何清空所有元素的函数、方法，
+			// 清空 map 的唯一办法就是重新 make 一个新的 map，
+			// 不用担心垃圾回收的效率，Go语言中的并行垃圾回收效率比写一个清空函数要高效的多。
+			counts = make(map[string] int)
+		}
+	}
+}
+
+func out_counts(counts map[string] int, f_name string){
+	for line, n := range counts{
+		if n > 1{
+			fmt.Printf("%d \t %s \t %s \n", n, line, f_name)
+		}
+	}
+}
+
+func countlines(f *os.File, counts map[string] int){
+	input := bufio.NewScanner(f)
+	for input.Scan(){
+		counts[input.Text()] ++
+	}
+}
 func main(){
-	cal_nums()
+	dup_files()
 }
