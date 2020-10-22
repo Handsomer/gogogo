210ea1a1d687dfd10e243713c230415fb69d100e
sunweisunwei <sunweisunwei@didiglobal.com>
2020-10-22 16:01:16 +0800

    详细查看调用过两次的原因

--- cp1/server_count.go
+++ cp1/server_count.go
@@ -13,13 +13,18 @@ var count int
 func main(){
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
-	http.HandleFunc("/favicon.ico", doNothing)
+	//http.HandleFunc("/favicon.ico", doNothing)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func doNothing(w http.ResponseWriter, r *http.Request){}
 
 func handler(w http.ResponseWriter, r *http.Request){
+	if r.URL.RequestURI() == "/favicon.ico"{
+		fmt.Printf("view favicon ico %d \n", count)
+	}else{
+		fmt.Printf("not view favicon ico %d \n", count)
+	}
 	mu.Lock()
 	count += 1
 	fmt.Fprintf(w, "URL.Path = %q %d \n", r.URL.Path, count)
